d2ee977da87cac329fdc241602c3cbdcc121794a
Divyam Anshumaan <41248177+divyam02@users.noreply.github.com>
2019-04-11 15:58:34 +0530

    Fixed velocity vectors and orientation
    Also added leader change option.

--- ArduCopter/Swarm Features/mimic_master.py
+++ ArduCopter/Swarm Features/mimic_master.py
@@ -4,29 +4,32 @@ from geometry_msgs.msg import PoseStamped, TwistStamped, Twist, Vector3, Pose
 from mavros_msgs.msg import PositionTarget, AttitudeTarget, State
 from mavros_msgs.srv import SetMode, SetModeRequest
 from std_msgs.msg import Float64
+import argparse
 
-def vel_callback(data):
-	vel2_pub = rospy.Publisher('/vehicle2/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
-	vel3_pub = rospy.Publisher('/vehicle3/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
-	vel4_pub = rospy.Publisher('/vehicle4/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
-	pub = TwistStamped()
-	print(data.twist.linear.x, data.twist.linear.z)
-	pub.twist.linear.x=data.twist.linear.x
-	pub.twist.linear.z=data.twist.linear.z
-	pub.twist.linear.y=data.twist.linear.y
-	pub.twist.angular.x=data.twist.angular.x
-	pub.twist.angular.y=data.twist.angular.y
-	pub.twist.angular.z=data.twist.angular.z
-	vel2_pub.publish(pub)
-	vel3_pub.publish(pub)
-	vel4_pub.publish(data)
+def vel_callback(data, args):
+	drone_count = args[0]
+	curr_leader = args[1]
+	for i in range(1, drone_count+1):
+		if i is not curr_leader:
+			rospy.Publisher('/vehicle'+str(i)+'/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10).publish(data)
 
-def moveSame():
-	rospy.Subscriber("/vehicle1/mavros/local_position/velocity_local", TwistStamped , vel_callback)
+def orient_callback(data, args)
+	drone_count = args[0]
+	for i in range(1, drone_count+1):
+		if i is not curr_leader:
+			rospy.Publisher('/vehicle'+str(i)+'/mavros/global_position/compass_hdg', Float64, queue_size=10).publish(data)
+
+def moveSame(drone_count, curr_leader=1):
+	rospy.Subscriber("/vehicle"+str(curr_leader)+"/mavros/local_position/velocity_local", TwistStamped , vel_callback, (drone_count, curr_leader))
+	rospy.Subscriber("/vehicle"+str(curr_leader)+"/mavros/global_position/compass_hdg", Float64, orient_callback, (drone_count))
 	
 	
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--n', dest='drone_count', type=int, default=1)
+	parser.add_argument('--leader', dest='curr_leader', type=int, default=1)
+	args = parser.parse_args()
 	rospy.init_node('circle', anonymous=True)
 	rospy.Rate(10)
-	moveSame()
+	moveSame(args.drone_count, args.curr_leader)
 	rospy.spin()
